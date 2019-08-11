85f5a0d08ae71239cd54dee83515900f48fe8530
Mustafa-W <30318235+Mustafa-W@users.noreply.github.com>
2019-08-11 21:24:51 +0200

    Updates

---
+++ precache-manifest.8135130b8ca12e7b264e86c413332d50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0a9e51987074007977980c1e2998a1",
+    "url": "/react-form-app/index.html"
+  },
+  {
+    "revision": "a343d580ef18dff3f9fd",
+    "url": "/react-form-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7784e65ca7e83511f311",
+    "url": "/react-form-app/static/js/2.20464c8a.chunk.js"
+  },
+  {
+    "revision": "a343d580ef18dff3f9fd",
+    "url": "/react-form-app/static/js/main.bfe92807.chunk.js"
+  },
+  {
+    "revision": "af4a6e55924e9a7d9578",
+    "url": "/react-form-app/static/js/runtime~main.0acba3e9.js"
+  }
+]);
